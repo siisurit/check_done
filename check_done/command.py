251388af2169ace8d0f2567d7180c82cc1736d39
Mek Skëndo <mekskendo@gmail.com>
2024-09-27 16:35:24 +0200

    #1 Fix project setup

--- check_done/command.py
+++ check_done/command.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 def main():
     logging.basicConfig(level=logging.WARNING)
-    logger.warning("All checks completed! /s")
+    logger.warning("Hello world!")
 
 
 if __name__ == "__main__":
