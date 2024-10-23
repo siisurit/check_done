a426b0c1d9a6bcfeca8017997507c60b172904b0
Mek Skëndo <mekskendo@gmail.com>
2024-10-23 19:17:59 +0200

    #5 Check assignees

--- check_done/checks.py
+++ check_done/checks.py
@@ -2,31 +2,41 @@ from check_done.done_project_items_info.done_project_items_info import done_proj
 from check_done.done_project_items_info.info import ProjectItemInfo
 
 
-def check_done_issues_for_warnings() -> list[str | None]:
+def check_done_project_items_for_warnings() -> list[str | None]:
     result = []
-    done_issues = done_project_items_info()
-    criteria_checks = [
-        _check_done_issues_are_closed,
-    ]
-    for issue in done_issues:
-        warnings = [check(issue) for check in criteria_checks if check(issue)]
-        result.extend(warnings)
+    done_project_items = done_project_items_info()
+    for project_item in done_project_items:
+        warning_reasons = [
+            warning_reason for check, warning_reason in CONDITION_CHECK_AND_WARNING_REASON_LIST if check(project_item)
+        ]
+        if len(warning_reasons) >= 1:
+            warning = _project_item_warning_string(project_item, warning_reasons)
+            result.append(warning)
     return result
 
 
-def _check_done_issues_are_closed(issue: ProjectItemInfo) -> str | None:
-    result = None
-    if not issue.closed:
-        result = _issue_warning_string(
-            issue,
-            "closed",
-        )
-    return result
-
-
-def _issue_warning_string(issue: ProjectItemInfo, reason_for_warning: str) -> str:
-    # TODO: Ponder better wording.
+def _project_item_warning_string(issue: ProjectItemInfo, reasons_for_warning: list[str]) -> str:
+    if len(reasons_for_warning) >= 3:
+        reasons_for_warning = f"{', '.join(reasons_for_warning[:-1])}, and {reasons_for_warning[-1]}"
+    elif len(reasons_for_warning) == 2:
+        reasons_for_warning = f"{', '.join(reasons_for_warning[:1])} and is {reasons_for_warning[1]}"
+    else:
+        reasons_for_warning = reasons_for_warning[0]
     return (
-        f" Done project item should be {reason_for_warning}."
+        f" Done project item is {reasons_for_warning}."
         f" - repository: '{issue.repository.name}', project item: '#{issue.number} {issue.title}'."
     )
+
+
+def _is_not_closed(project_item: ProjectItemInfo) -> bool:
+    return not project_item.closed
+
+
+def _is_not_assigned(project_item: ProjectItemInfo) -> bool:
+    return project_item.assignees.total_count < 1
+
+
+CONDITION_CHECK_AND_WARNING_REASON_LIST = [
+    (_is_not_closed, "not closed"),
+    (_is_not_assigned, "missing assignee"),
+]
